e908254ccd4ceb4a5367399ae51db84127892225
Ales-fox <Alex-170395@mail.ru>
2022-05-28 19:36:48 +0300

    рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const popupContainerEdit = document.querySelector('.popup__container_edit');
 const popupContainerAdd = document.querySelector('.popup__container_add');
 const popupPhoto = document.querySelector('.popup_photo');
 const buttonEdit = profile.querySelector('.button-edit');
-const buttonsClose = document.querySelectorAll('.button-close');
+const buttonCloses = document.querySelectorAll('.button-close');
 const buttonAdd = profile.querySelector('.button-add');
 const cards = document.querySelector('.cards');
 const templateCard = document.querySelector('.template__card').content;
@@ -41,6 +41,8 @@ const inputNamePlace = popupFormAdd.querySelector('.input_name-of-place');
 const inputURL = popupFormAdd.querySelector('.input_url');
 const profileName = profile.querySelector('.profile__name');
 const profileStatus = profile.querySelector('.profile__status');
+const popupPhotoBig = popupPhoto.querySelector('.popup__bigPhoto');
+const popupPhotoSubtitle = popupPhoto.querySelector(".popup__subtitle");
 
 /*Отображение 6 начальных карточек на странице*/
 function renderCard({ name, link }) {
@@ -59,51 +61,52 @@ function createCard({ name, link }) {
         const evtTarget = evt.target.closest('.card');
         evtTarget.remove();
     });
-    newCard.querySelector('.card__photo').addEventListener('click', function (evt) {
+    newCard.querySelector('.card__photo').addEventListener('click', function () {
         openPopup(popupPhoto);
-        openPhoto(evt);
+        openPhoto(name, link);
     });
     titleCard.textContent = name;
     photoLinkCard.src = link;
     photoLinkCard.alt = name;
     return newCard;
 }
+
 /*Открытие Popup*/
 function openPopup(popup) {
     popup.classList.add('popup_opened');
 }
 
 /* Увеличение фото*/
-function openPhoto(evt) {
-    evt.preventDefault();
-    const evtTarget = evt.target;
-    const bigPhoto = popupPhoto.querySelector('.popup__bigPhoto');
-    bigPhoto.src = evtTarget.src;
-    bigPhoto.alt = evtTarget.alt;
-    const subtitle = popupPhoto.querySelector(".popup__subtitle");
-    subtitle.textContent = evtTarget.alt;
-    popupPhoto.classList.add('popup_opened');
+function openPhoto(name, link) {
+    popupPhotoBig.src = link;
+    popupPhotoBig.alt = name;
+    popupPhotoSubtitle.textContent = name;
+    openPopup(popupPhoto);
 }
-/*Закрытие Popup*/
-function closePopup(evt) {
-    evt.target.closest('.popup').classList.remove('popup_opened');
+
+function hideClosestPopup(evt) {
+    closePopup(evt.target.closest('.popup'));
 }
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+
 /*Сохранение и отсылка данных формы редактирования на сервер*/
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileStatus.textContent = inputStatus.value;
-    closePopup(evt);
+    hideClosestPopup(evt);
 }
 /*Сохранение данных и последующее добавление новой карточки*/
-function formAddsubmit(evt) {
+function handleCardFormSubmit(evt) {
     evt.preventDefault();
     const name = inputNamePlace.value;
     const link = inputURL.value;
     cards.prepend(createCard({ name, link }));
-    inputNamePlace.value = ''; /* Очищение инпутов для послед. доб. карточек*/
-    inputURL.value = '';
-    closePopup(evt);
+    evt.target.reset();
+    hideClosestPopup(evt);
 }
 
 initialCards.forEach(renderCard);
@@ -115,6 +118,6 @@ buttonEdit.addEventListener('click', function () {
 buttonAdd.addEventListener('click', function () {
     openPopup(popupFormAdd);
 });
-buttonsClose.forEach((buttonClose) => buttonClose.addEventListener('click', closePopup));
-popupContainerEdit.addEventListener('submit', formSubmitHandler);
-popupContainerAdd.addEventListener('submit', formAddsubmit);
+buttonCloses.forEach((buttonClose) => buttonClose.addEventListener('click', hideClosestPopup));
+popupContainerEdit.addEventListener('submit', handleProfileFormSubmit);
+popupContainerAdd.addEventListener('submit', handleCardFormSubmit);
